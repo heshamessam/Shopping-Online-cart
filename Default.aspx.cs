5562e6066e4a6287c180b88bf7f4059cf47eccda
Hesham Essam <heshamessam090@gmail.com>
2017-06-05 01:57:11 +0200

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using ShoppingHeart.BusinessLayer;
+
+namespace ShoppingHeart
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                lblCategoryName.Text = "Popular Products At ShoppingHeart";
+                GetCategory();
+                GetProducts(0);
+            }
+            lblAvailableStockAlert.Text = string.Empty;
+        }
+
+        private void GetCategory()
+
+        {
+            ShoppingCart k = new ShoppingCart();
+            dlCategories.DataSource = null;
+            dlCategories.DataSource = k.GetCategories();
+            dlCategories.DataBind(); 
+        }
+
+        private void GetProducts(int CategoryID)
+        {
+            ShoppingCart k = new ShoppingCart()
+            {
+                CategoryID = CategoryID
+            };
+
+            dlProducts.DataSource = null;
+            dlProducts.DataSource = k.GetAllProducts();
+            dlProducts.DataBind();
+        }
+
+        protected void lbtnCategory_Click(object sender, EventArgs e)
+        {
+            pnlMyCart.Visible = false;
+            pnlProducts.Visible = true;
+            int CategoryID = Convert.ToInt16((((LinkButton)sender).CommandArgument));
+            GetProducts(CategoryID);
+        }
+
+        protected void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            string ProductID = Convert.ToInt16((((Button)sender).CommandArgument)).ToString();
+            string ProductQuantity = "1";
+            DataListItem currentItem = (sender as Button).NamingContainer as DataListItem;
+            Label lblAvailableStock = currentItem.FindControl("lblAvailableStock") as Label;
+
+            if (Session["MyCart"] != null)
+            {
+                DataTable dt = (DataTable)Session["MyCart"];
+                var checkProduct = dt.AsEnumerable().Where(r => r.Field<string>("ProductID") == ProductID);
+                if(checkProduct.Count() == 0)
+                {
+                    string query = select * from Products where ProductID =  + ProductID + "";
+                    DataTable dtProducts = GetData(query);
+
+                    DataRow dr = dt.NewRow();
+                    dr["ProductID"] = ProductID;
+                    dr["Name"] = Convert.ToString(dtProducts.Rows[0]["Name"]);
+                    dr["Description"] = Convert.ToString(dtProducts.Rows[0]["Description"]);
+                    dr["Price"] = Convert.ToString(dtProducts.Rows[0]["Price"]);
+                    dr["ImageUrl"] = Convert.ToString(dtProducts.Rows[0]["ImageUrl"]);
+                    dr["ProductQuantity"] = ProductQuantity;
+                    dr["AvailableStock"] = lblAvailableStock.Text;
+                    dt.Rows.Add(dr);
+
+                }
+            }
+
+        }
+    }
+}
